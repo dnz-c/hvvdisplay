f4ff3e160bec3b588f7d5f005b572f76eca5f471
ItzPAX <54080059+dnz-c@users.noreply.github.com>
2026-06-13 22:48:49 +0200

    account for long distance train delays

--- hvv_display.py
+++ hvv_display.py
@@ -116,7 +116,7 @@ class TrainBoard:
                     "line": line_name,
                     "dest": direction,
                     "time": time_str,
-                    "is_delayed": delay_minutes > (3 if line_name[0] == "S" else 1)
+                    "is_delayed": delay_minutes > (1 if line_name[0] == "U" else 5)
                 })
         except Exception as e:
             print(f"API Fehler: {e}")
